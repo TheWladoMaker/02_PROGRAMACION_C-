a29059c3e2e4c1408a285b27f57d232fbde4549f
TheWladoMaker <59052083+TheWladoMaker@users.noreply.github.com>
2020-02-04 20:54:59 -0300

    Add files via upload

---
+++ CLASE_1.cpp
@@ -0,0 +1,16 @@
+// primer programa en C++
+/* el primer programa en C++*/
+#include<iostream> //input ouput stream entrada y salida de datos 
+                   //es una libreria que sirve por que gracias a ella 
+                   //va a tener entrada y salida de datos
+#include <conio.h>
+using namespace std;
+
+int main(){
+	
+	cout<<"\n Hola mundo listo para la aventura - si puedes imaginarlo, puedes programarlo \n"<<endl;
+	cout<<"\n aqui estoy\n"<<endl;
+	
+		getchar();
+    return 0;	
+}
